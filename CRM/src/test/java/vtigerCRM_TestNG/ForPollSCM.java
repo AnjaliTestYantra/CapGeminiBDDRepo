0d47b91b1a0ca284db32898bdad9a9a60cb81945
AnjaliTestYantra <anjali.paleth@gmail.com>
2025-08-13 18:26:31 +0530

    sdfghj

---
+++ CRM/src/test/java/vtigerCRM_TestNG/ForPollSCM.java
@@ -0,0 +1,11 @@
+package vtigerCRM_TestNG;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ForPollSCM {
+@Test
+public void sampleTest() {
+	Reporter.log("Success", true);
+}
+}
